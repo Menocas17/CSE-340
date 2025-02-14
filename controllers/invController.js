0f6e95ce04382431681c6390d0fe8e1021c387c5
Menocas17 <rodolfomenocal7@gmail.com>
2025-02-14 16:09:10 -0600

    adding the routes, controllers and views for the management, new classificationa nd new inventory

--- controllers/invController.js
+++ controllers/invController.js
@@ -11,7 +11,7 @@ invCont.buildByClassificationId = async function (req, res, next) {
     const grid = await utilities.buildClassificationGrid(data)
     let nav = await utilities.getNav()
     const className = data[0].classification_name
-    res.render('./inventory/classification', {
+    res.render('inventory/classification', {
         title: className + ' vehicles',
         nav,
         grid,
@@ -26,11 +26,47 @@ invCont.buildVehicleDetailsById = async function (req, res, next) {
     const detailsView = await utilities.buildVehicleDetailsView(data)
     let nav = await utilities.getNav()
     const title = `${data[0].inv_year} ${data[0].inv_make} ${data[0].inv_model}`
-    res.render('./inventory/details', {
+    res.render('inventory/details', {
         title: title,
         nav,
         detailsView
     })
 }
+
+// Build the management view 
+
+invCont.buildManagementView = async function (req, res, next) {
+    let nav = await utilities.getNav();
+    res.render('inventory/management', {
+        title: 'Vehicle Management',
+        nav,
+        errors: null,
+    })
+}
+
+// Build the new classification view 
+
+invCont.buildNewClassificationtView = async function (req, res, next) {
+    let nav = await utilities.getNav();
+    res.render('inventory/newClassification', {
+        title: 'Add New Classification',
+        nav,
+        errors: null,
+    })
+}
+
+//Build the new inventory view 
+invCont.buildNewInventoryView = async function (req, res, next) {
+    let nav = await utilities.getNav();
+    const classData = await invModel.getClassifications()
+    const classifications = classData.rows
+    res.render('inventory/newInventory', {
+        title: 'Add New Inventory',
+        nav,
+        classifications,
+        errors: null,
+    })
+}
+
 
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -9,4 +9,10 @@ router.get('/type/:classificationId', utilities.handleErrors(invController.build
 // Route for the deatiled vehicle view 
 router.get('/detail/:inv_id', utilities.handleErrors(invController.buildVehicleDetailsById));
 
+router.get('', utilities.handleErrors(invController.buildManagementView))
+
+router.get('/newClassification', utilities.handleErrors(invController.buildNewClassificationtView))
+
+router.get('/newInventory', utilities.handleErrors(invController.buildNewInventoryView))
+
 module.exports = router;
